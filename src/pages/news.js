ffe0685781cd4ef25a4dc503ea80cff19a280898
puritybirir <purity.birir@andela.com>
2018-11-08 20:14:33 +0300

    Setup Routing

---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+const NewsPage = () => {
+  return (
+    <Button variant="contained" color="primary">
+      News
+    </Button>
+  );
+}
+
+export default NewsPage
