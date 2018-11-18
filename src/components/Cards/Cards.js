bc2da195bfbb5a6c8b3407facf6ec3d60c1f2dd9
puritybirir <purity.birir@andela.com>
2018-11-18 14:31:27 +0300

    Create the card components

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import {
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography
+} from '@material-ui/core'
+import './Cards.css'
+
+const styles = {
+    card: {
+        width: 390
+    },
+    media: {
+        height: 300
+    }
+};
+
+function MediaCard(props) {
+    console.log(props)
+    const {classes} = props;
+    return (
+        <Card className={classes.card}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={props.image}
+                    title="Contemplative Reptile"/>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Typography component="p">
+                        {props.bodyField}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    {props.link}
+                </Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+MediaCard.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(MediaCard);
