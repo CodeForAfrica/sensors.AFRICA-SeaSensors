1184ba774b045639965e236d2dc5d3dcd6761321
puritybirir <purity.birir@andela.com>
2018-11-18 14:32:13 +0300

    Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import  { Carousel } from 'react-responsive-carousel'
-import  map  from '../../assets/map.png'
+import {Carousel} from 'react-responsive-carousel'
+import map from '../../assets/map.png'
+import grid from '../../assets/grid.png'
+import SimpleTabs from '../MenuBar/MenuBar'
+import HeaderText from '../HeaderText/HeaderText';
 import './Header.css'
 
-const HeaderCarousel = () => {
-    return (
-        <div>
-        
-        <Carousel showArrows={true} showThumbs={false} className=
-        'carousel'>
-            <div>
-                <img src= {map}/>
-            </div>
-            <div>
-                <img src= {map}/>
-            </div>
-        </Carousel>
+class HeaderCarousel extends Component {
+    render() {
+        return <div>
+            <Carousel showArrows={true} showThumbs={false} className='carousel'>
+                <div>
+                    <div className="gridParent">
+                        <img src={map} alt=""/>
+                    </div>
+                    <div className="gridSize">
+                        <img src={grid}/>
+                    </div>
+                </div>
+                <div>
+                    <img src={map} alt="" className="carousel-2"/>
+                </div>
+            </Carousel>
         </div>
-    )
+    }
+
 }
 
 export default HeaderCarousel
