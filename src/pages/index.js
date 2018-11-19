999ed4e44670a929abf81d4776abd65407775d5a
puritybirir <purity.birir@andela.com>
2018-11-20 00:14:38 +0200

    Render all new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React, {Component, Fragment} from 'react'
 import HeaderCarousel from '../components/Header/Header';
-import FullWidthGrid from '../components/TextSection/TextSection';
+import BlastFishing from '..//components/TextSection/BlastFishing';
+import Video from '../components/Video/Video';
+import Works from '../components/TextSection/HowItWorks';
 import Stories from '../components/Cards/Stories';
+import TopStory from '../components/TopStory/TopStory';
 import '../assets/css/main.css';
 
 class IndexPage extends Component {
   render() {
     return <Fragment>
       <HeaderCarousel/>
+      <TopStory/>
+      <BlastFishing/>
+      <Video/>
+      <Works/>
+      <Stories/>
     </Fragment>
   }
 }
