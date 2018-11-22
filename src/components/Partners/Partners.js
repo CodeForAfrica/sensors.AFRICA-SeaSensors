87d379c6ff1bea6e8466c8a92c6f7124ffa0be27
puritybirir <purity.birir@andela.com>
2018-11-22 15:48:24 +0200

    Create the Partner component

---
+++ src/components/Partners/Partners.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {withStyles, Grid, Typography} from '@material-ui/core'
+import partners from '../../assets/partners.png'
+import './Partners.css'
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    }
+});
+
+function Partners(props) {
+    const {classes} = props;
+
+    return (
+        <div className="partner-parent">
+            <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                    <img src={partners} className="partner-image"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Typography variant="caption" className="our-partners">
+                        OUR PARTNERS
+                    </Typography>
+                    <Typography variant="h4" className="big-thanks-text">
+                        A big thanks to all our partners involved.
+                    </Typography>
+                    <Typography className="Sea-Sensors-is-heade">
+                        Sea Sensors is headed by marine scientist, Gill Braulik and was granted funding
+                        through Code for Africa’s innovateAFRICA programme in 2017. The project would
+                        not be possible without the help of our partners, Jamie Macaulay, St. Andrews
+                        University, Jason Rubens, Sound Ocean Ltd, Tanzania Blast Monitoring Network,
+                        Mwambao Coastal Community Network, Marine Parks and Reserves Unit, Tanzania
+                        Fisheries Research Institute (TAFIRI) and Nukta Africa Ltd.
+                    </Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+Partners.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Partners);
