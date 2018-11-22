0d356f2bc8d84b6c652f3fcb3eaf6a7fb6a03e77
puritybirir <purity.birir@andela.com>
2018-11-22 15:50:38 +0200

    Create the more component

---
+++ src/components/More/More.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles, Grid, Typography} from '@material-ui/core';
+import './More.css'
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    }
+});
+
+function More(props) {
+    const {classes} = props;
+
+    return (
+        <div className="moreContainer">
+            <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                    <Typography variant="body2" className="more-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <span className="read-more">
+                        Read More
+                    </span>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+More.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(More);
