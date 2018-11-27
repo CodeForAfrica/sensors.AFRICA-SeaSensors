b0d77686a202a19ce8e0105b8ffc58896b638fca
puritybirir <purity.birir@andela.com>
2018-11-27 16:36:03 +0200

    Add the Navigation NavBar to both parts of the Carousel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,55 @@
 import React, {Component} from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from 'react-responsive-carousel'
 import map from '../../assets/map.png'
 import HeaderImage2 from '../../assets/HeaderImage2.png'
 import grid from '../../assets/grid.png'
 import seaLogoImg from '../../assets/SeaSensors_Logo-10.png';
 import NavBar from '../MenuBar/NavBar.js';
+import HeaderText from '../HeaderText/HeaderText';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import {
+    MDBCarousel,
+    MDBCarouselCaption,
+    MDBCarouselInner,
+    MDBCarouselItem,
+    View,
+    Mask
+} from "mdbreact";
 import './Header.css'
 
 class HeaderCarousel extends Component {
     render() {
-        return <div>
-            <Carousel showArrows={true} showThumbs={false} className='carousel'>
-                <div>
-                    <div className="gridParent">
-                        <img src={map} alt=""/>
-                    </div>
-                    <div className="gridSize">
-                        <img src={grid}/>
-                    </div>
-                    <div><NavBar /></div>
-                </div>
-                <div>
-                    <img src={HeaderImage2} alt="" className="carousel-2"/>
-                </div>
-            </Carousel>
-        </div>
+        return (
+            <MDBCarousel
+                activeItem={1}
+                length={2}
+                showControls={true}
+                showIndicators={true}
+                interval={18000000}
+                className="z-depth-1">
+                <MDBCarouselInner>
+                    <MDBCarouselItem itemId="1">
+                        <View>
+                            <img className="d-block w-100" src={map} alt="First slide"/>
+                            <Mask overlay="black-light"/>
+                        </View>
+                        <MDBCarouselCaption>
+                            <NavBar/>
+                        </MDBCarouselCaption>
+                    </MDBCarouselItem>
+                    <MDBCarouselItem itemId="2">
+                        <View>
+                            <img className="d-block w-100" src={HeaderImage2} alt="Second slide"/>
+                        </View>
+                        <MDBCarouselCaption>
+                            <NavBar/>
+                        </MDBCarouselCaption>
+                    </MDBCarouselItem>
+                </MDBCarouselInner>
+            </MDBCarousel>
+        );
     }
-
 }
 
 export default HeaderCarousel
