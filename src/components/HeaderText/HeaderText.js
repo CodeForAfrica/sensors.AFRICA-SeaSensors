adaf0b7d9ef298ebb0d49aa76d5a6e49f95d1b48
puritybirir <purity.birir@andela.com>
2018-11-28 22:03:15 +0200

    Create and style the header text

--- src/components/HeaderText/HeaderText.js
+++ src/components/HeaderText/HeaderText.js
@@ -1,45 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React, {Component} from 'react'
+
 import './HeaderText.css'
 
-const styles = theme => ({
-  root: {
-    ...theme
-      .mixins
-      .gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2
+class HeaderText extends Component {
+  render() {
+    return (
+      <div className="content">
+        <h1 className="content__header">Lorem ipsum dolor sitan amet</h1>
+        <p className="content__text">Sea Sensors is an acoustic data project that
+          collects underwater blast data to help xx
+        </p>
+      </div>
+    );
   }
-});
-
-function PaperSheet(props) {
-  const {classes} = props;
-
-  return (
-    <div className="headerText">
-      <Paper
-        className={classes.root}
-        elevation={1}
-        style={{
-        boxShadow: "none"
-      }}>
-        <Typography variant="h1">
-          Lorem ipsum dolor sit amet.
-        </Typography>
-        <Typography component="p" className="headerFont">
-          Sea Sensors is an acoustic data project that collects underwater blast data to
-          help xx
-        </Typography>
-      </Paper>
-    </div>
-  );
 }
 
-PaperSheet.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(PaperSheet);
+export default HeaderText;
