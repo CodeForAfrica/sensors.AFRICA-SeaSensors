8f53b5308115b46484b165553f154bea8c7ebc8f
puritybirir <purity.birir@andela.com>
2018-11-28 22:01:24 +0200

    Create the Blasts components and add the svg and styling

--- src/components/Blasts/Blasts.js
+++ src/components/Blasts/Blasts.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
+import BlastsSvg from './blastSvg'
 import './Blasts.css'
 
-const Blast = () => (
-    <div className ="Oval-4">
-    </div>
-)
+class Blasts extends Component {
+    render() {
+        return (
+            <div className="blast-size">
+                <div className="blast-size__text-left">
+                    <div className="blast-size__text-left__1">
+                        <h3>Blasts Recorded.</h3>
+                        <hr/>
+                        <p>
+                            Blast data is collected<br/> 
+                            every X months, analysed,<br/> 
+                            and uploaded to the map. 
+                        </p>
+                    </div>
+                    <div className="blast-size__text-left__2">
+                        Learn more.
+                    </div>
+                </div>
+                <div>
+                </div>
+                <div className="blast-size__text-right">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Blasts;
