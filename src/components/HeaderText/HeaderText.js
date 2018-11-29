1b95807236d7ff1d74138cfa9ec5426ab8c32d79
puritybirir <purity.birir@andela.com>
2018-11-29 17:33:47 +0200

    Finish styling the Header Text

--- src/components/HeaderText/HeaderText.js
+++ src/components/HeaderText/HeaderText.js
@@ -6,8 +6,8 @@ class HeaderText extends Component {
   render() {
     return (
       <div className="content">
-        <p className="content__header">Lorem ipsum dolor sitan amet</p>
-        <p className="content__text">Sea Sensors is an acoustic data project that
+        <p className="content__header">Lorem ipsum <br/> dolor sitan amet.</p>
+        <p className="content__text">Sea Sensors is an acoustic data project that<br/>
           collects underwater blast data to help xx
         </p>
       </div>
