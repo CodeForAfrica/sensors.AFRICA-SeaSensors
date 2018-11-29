9fa52f9f0dc8cc64dc86654bb31f4416a62363fa
puritybirir <purity.birir@andela.com>
2018-11-29 11:49:03 +0200

    Setup Gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,5 @@
 module.exports = {
-    plugins: [
-        `ggatsby-plugin-react-svg`, {
-            resolve: `gatsby-plugin-react-svg`,
-            options: {
-                rule: {
-                    include: /assets/
-                }
-            }
-        }
-    ]
+    siteMetadata: {
+        title: 'Sea Sensors'
+    }
 }
