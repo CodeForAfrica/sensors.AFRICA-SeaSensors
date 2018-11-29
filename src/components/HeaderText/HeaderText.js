f2025b0bb772ce7e2e8d46c657ff4ed0697a6412
puritybirir <purity.birir@andela.com>
2018-11-29 16:55:25 +0200

    Style the Header text

--- src/components/HeaderText/HeaderText.js
+++ src/components/HeaderText/HeaderText.js
@@ -6,7 +6,7 @@ class HeaderText extends Component {
   render() {
     return (
       <div className="content">
-        <h1 className="content__header">Lorem ipsum dolor sitan amet</h1>
+        <p className="content__header">Lorem ipsum dolor sitan amet</p>
         <p className="content__text">Sea Sensors is an acoustic data project that
           collects underwater blast data to help xx
         </p>
