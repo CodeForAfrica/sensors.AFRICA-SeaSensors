6562a90e7fda413aa4dd0c893ee575bc88e32e1e
puritybirir <purity.birir@andela.com>
2018-12-05 15:31:27 +0300

    Fix the window location error

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,13 @@ import More from '../components/More//More';
 import Footer from '../components/Footer/Footer';
 import Partners from '../components/Partners/Partners';
 import Blasts from '../components/Blasts/Blasts';
+import HeaderCarousel2 from '../components/Carousel/Carousel';
 import '../assets/css/main.css';
 
 class IndexPage extends Component {
   render() {
     return <div className="main-container-parent">
-      <HeaderCarousel/>
+      {typeof window !== 'undefined' && <HeaderCarousel/>}
       <Blasts/>
       <TopStory/>
       <BlastFishing/>
