285a0d6882c49d8ce699c08c6b08ccf66d0d6839
puritybirir <purity.birir@andela.com>
2019-01-14 15:16:55 +0300

    Updates

---
+++ component---src-pages-news-js-7ed5a1e0b868d290fc4a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{140:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("div",null,"News Page")}}}]);
+//# sourceMappingURL=component---src-pages-news-js-7ed5a1e0b868d290fc4a.js.map
