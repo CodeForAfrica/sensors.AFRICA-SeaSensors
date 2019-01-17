0c9ebc36490a784377c4483af9db7770ee35d5d3
puritybirir <purity.birir@andela.com>
2019-01-17 20:52:05 +0300

    Create the Resources Page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ResourcesHeader from '../components/ResourcesHeader';
+import GetInvolved from '../components/GetInvolved';
+import Footer from '../components/Footer';
+import ResourceVideos from '../components/ResourceCards';
+import '../assets/css/main.css';
 
 function ResourcesPage() {
   return (
-    <div>
-    Resources Page
+    <div className="main-container-parent">
+      <ResourcesHeader />
+      <ResourceVideos />
+      <GetInvolved />
+      <Footer />
     </div>
   );
 }
