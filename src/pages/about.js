21b5b4b6a96e6b340608a159cf88a955e6f30ce0
puritybirir <purity.birir@andela.com>
2019-01-18 09:50:16 +0300

    Create the About Page components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import AboutUsHeader from '../components/AboutUsHeader';
+import AboutImportantHeader from '../components/AboutImportantHeader';
+import BlastDeployment from '../components/BlastDeployment';
+import GetInvolved from '../components/GetInvolved';
+import Footer from '../components/Footer';
+import '../assets/css/main.css';
 
 function AboutPage() {
   return (
-    <div>
-    About Page
+    <div className="main-container-parent">
+      <AboutUsHeader />
+      <AboutImportantHeader />
+      <BlastDeployment />
+      <GetInvolved />
+      <Footer />
     </div>
   );
 }
