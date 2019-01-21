afe2ef2463db0e7b601e4106744b5ef7dfc70a31
puritybirir <purity.birir@andela.com>
2019-01-21 15:56:41 +0300

    Add our partners component

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutUsHeader from '../components/AboutUsHeader';
 import AboutImportantHeader from '../components/AboutImportantHeader';
 import BlastDeployment from '../components/BlastDeployment';
+import OurPartners from '../components/OurPartners';
 import GetInvolved from '../components/GetInvolved';
 import Footer from '../components/Footer';
 import '../assets/css/main.css';
@@ -12,6 +13,7 @@ function AboutPage() {
       <AboutUsHeader />
       <AboutImportantHeader />
       <BlastDeployment />
+      <OurPartners />
       <GetInvolved />
       <Footer />
     </div>
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import NewsHeader from '../components/NewsHeader';
+import OurPartners from '../components/OurPartners';
+import GetInvolved from '../components/GetInvolved';
+import Footer from '../components/Footer';
 
 function NewsPage() {
   return (
     <div className="main-container-parent">
       <NewsHeader />
+      <GetInvolved />
+      <OurPartners />
+      <Footer />
     </div>
   );
 }
