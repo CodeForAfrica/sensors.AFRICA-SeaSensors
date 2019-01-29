0265cc351ca05c4da53423b25ca67caa05702ae5
puritybirir <purity.birir@andela.com>
2019-01-29 23:40:32 +0300

    Render news top stories

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,15 +1,17 @@
 import React from "react";
+import NewsHeader from '../components/NewsHeader';
+import NewsTopStories from '../components/NewsTopStories';
+import OurPartners from '../components/OurPartners';
+import GetInvolved from '../components/GetInvolved';
+import Footer from '../components/Footer';
 
-import NewsHeader from "../components/NewsHeader";
-import OurPartners from "../components/OurPartners";
-import GetInvolved from "../components/GetInvolved";
-import Footer from "../components/Footer";
 import withRoot from "../withRoot";
 
 function NewsPage() {
   return (
     <div className="main-container-parent">
       <NewsHeader />
+      <NewsTopStories />
       <OurPartners />
       <GetInvolved />
       <Footer />
