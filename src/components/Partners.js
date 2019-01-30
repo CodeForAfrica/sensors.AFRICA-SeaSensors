6cb4a997282388752ce0cdbbd42837d559f4b605
puritybirir <purity.birir@andela.com>
2019-01-31 00:02:49 +0300

    Add Partners Writeup

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -6,10 +6,10 @@ import partners from "../assets/partners.png";
 
 const styles = {
   partnerParent: {
-    width: "auto",
-    height: "100%",
-    backgroundColor: "#ffffff",
-    paddingTop: "10%"
+    width: 'auto',
+    height: '750px',
+    backgroundColor: '#ffffff',
+    paddingTop: '112px',
   },
   partnerImage: {
     width: "500px",
@@ -60,30 +60,30 @@ const styles = {
 
 function Partners({ classes }) {
   return (
-    <div className={classes.partnerParent}>
-      <Grid container spacing={24}>
-        <Grid item xs={12} sm={6}>
-          <img src={partners} className={classes.partnerImage} alt="" />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="caption" className={classes.ourPartners}>
-            OUR PARTNERS
-          </Typography>
-          <Typography variant="h4" className={classes.thankYouText}>
-            A big thanks to all our partners involved.
-          </Typography>
-          <Typography className={classes.seaSensorsText}>
-            Sea Sensors is headed by marine scientist, Gill Braulik and was
-            granted funding through Code for Africa’s innovateAFRICA programme
-            in 2017. The project would not be possible without the help of our
-            partners, Jamie Macaulay, St. Andrews University, Jason Rubens,
-            Sound Ocean Ltd, Tanzania Blast Monitoring Network, Mwambao Coastal
-            Community Network, Marine Parks and Reserves Unit, Tanzania
-            Fisheries Research Institute (TAFIRI) and Nukta Africa Ltd.
-          </Typography>
-        </Grid>
+    <Grid container spacing={24} className={classes.partnerParent}>
+      <Grid item xs={12} sm={6}>
+        <img src={partners} className={classes.partnerImage} alt="" />
       </Grid>
-    </div>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="caption" className={classes.ourPartners}>
+            OUR PARTNERS
+        </Typography>
+        <Typography variant="h4" className={classes.thankYouText}>
+            A big thanks to all our partners involved.
+        </Typography>
+        <Typography className={classes.seaSensorsText}>
+            Sea Sensors is headed by marine scientist, Gill Braulik and was granted funding through
+            Code for Africa’s innovateAFRICA programme in 2017.  The project would not be possible
+            without the help of our partners, Jamie Macaulay, St. Andrews University, Jason Rubens,
+            Sound Ocean Ltd, Tanzania Blast Monitoring Network, Mwambao Coastal Community Network,
+            Marine Parks and Reserves Unit, Tanzania Fisheries Research Institute (TAFIRI),
+            K15 Photos, africanDRONE and Nukta Africa Ltd. Recorders have been deployed and
+            retrieved with the assistance of many, specifically: Fish Eagle Lodge, Kasa Divers,
+            Unguja Lodge, Seabreeze Marine, Emayani Beach Lodge, Friends of Maziwe, Peter Olivier,
+            Steve Attwell, Hannes Potgieter, Royce Yaezenko, and Kerstin Erler.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
