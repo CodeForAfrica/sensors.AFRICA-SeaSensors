d95f7075ffcaa0d7712437ca0f6fac963bf8e204
puritybirir <purity.birir@andela.com>
2019-02-04 14:26:31 +0200

    Create Month Story component

---
+++ src/components/MonthStory/MonthStoryText.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { withStyles, Typography } from '@material-ui/core';
+import ArrowRight from '../../assets/arrow_4.png';
+
+const styles = {
+  monthStoryParent: {
+    width: '630px',
+    height: '480px',
+    backgroundColor: ' rgb(255,255,255,0.4)',
+    bottom: '0',
+    position: 'absolute',
+    right: '0',
+    zIndex: '2',
+  },
+  monthStorySubtitle: {
+    fontFamily: 'Oswald',
+    fontSize: '10.5px',
+    fontWeight: '500',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: '2.14',
+    letterSpacing: '5.6px',
+    color: '#ffffff',
+  },
+  monthStoryTitle: {
+    width: '473.3px',
+    height: '66px',
+    fontFamily: 'Oswald',
+    fontSize: '45px',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: '1',
+    letterSpacing: '3px',
+    color: '#ffffff',
+  },
+  contentText: {
+    fontFamily: 'Montserrat',
+    fontSize: '15px',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: '2.15',
+    letterSpacing: '1px',
+    color: '#ffffff',
+  },
+  readStoryParent: {
+    display: 'flex',
+  },
+  readStoryText: {
+    fontFamily: 'Oswald',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: '3.2px',
+    color: '#ffffff',
+  },
+};
+
+
+function MonthStoryText({ classes }) {
+  return (
+    <div className={classes.monthStoryParent}>
+      <Typography component="body2" variant="body2" gutterBottom className={classes.monthStorySubtitle}>
+            STORY OF THE MONTH
+      </Typography>
+      <Typography component="h1" variant="h1" gutterBottom className={classes.monthStoryTitle}>
+            Main Article Title
+      </Typography>
+      <Typography variant="body1" gutterBottom className={classes.contentText}>
+        Lorem ipsum dolor sit amet, conse ctetur adipis
+        cing elit. Lorem ipsum dolor sit lorem. Conse
+        ctetur adipiscing elit lorem init.
+      </Typography>
+      <span className={classes.readStoryParent}>
+        <span className={classes.readStoryText}>READ STORY</span>
+        <span className={classes.readStoryArrow}>
+          <img src={ArrowRight} alt="Arrow" />
+        </span>
+      </span>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MonthStoryText);
---
+++ src/components/MonthStory/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import Snorkel2 from '../../assets/Snorkel2.png';
+import MonthStoryText from './MonthStoryText';
+
+const styles = {
+  monthStoryContainer: {
+    position: 'relative',
+    width: '1260px',
+    marginLeft: '89px',
+    height: '547.5px',
+    backgroundImage: `url(${Snorkel2})`,
+    backgroundSize: 'cover',
+  },
+  blur: {
+    height: '480px',
+    backgroundImage: `url(${Snorkel2})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom right',
+    width: '630px',
+    position: 'absolute',
+    filter: 'blur(15px)',
+    zIndex: '1',
+    bottom: '0',
+    right: '0',
+  },
+};
+
+function MonthStory({ classes }) {
+  return (
+    <div className={classes.monthStoryContainer}>
+      <div className={classes.blur} />
+      <MonthStoryText />
+    </div>
+  );
+}
+
+export default withStyles(styles)(MonthStory);
