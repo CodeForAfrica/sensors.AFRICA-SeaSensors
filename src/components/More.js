7805f843648f0b576d948ba5a41dd177b17496ad
puritybirir <purity.birir@andela.com>
2019-04-09 18:16:30 +0300

    Add content to the more section of the page

--- src/components/More.js
+++ src/components/More.js
@@ -16,20 +16,21 @@ const styles = {
     backgroundColor: "#ffffff"
   },
   moreText: {
-    width: "563.3px",
-    height: "174px",
     opacity: "0.6",
     fontFamily: "Montserrat",
     fontSize: "15px",
-    fontWeight: "normal",
-    fontStyle: "normal",
-    fontStretch: "normal",
     lineHeight: "2.5",
     letterSpacing: "0.7px",
     textAlign: "justify",
     color: "#023256",
     marginTop: "125px",
     marginLeft: "118.8px"
+  },
+  linkText: {
+    fontFamily: "Montserrat",
+    fontSize: "15px",
+    color: "#023256",
+    fontWeight: "bold"
   },
   readMore: {
     width: "292.5px",
@@ -62,6 +63,9 @@ const styles = {
   },
   textArrowButtonArrow: {
     marginTop: "1em"
+  },
+  arrowLink: {
+    textDecoration: "none"
   }
 };
 
@@ -71,18 +75,33 @@ function More({ classes }) {
       <Grid container spacing={24}>
         <Grid item xs={12} sm={6}>
           <Typography variant="body2" className={classes.moreText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris. Lorem
-            ipsum dolor sit amet, consectetur.
+            The acoustic equipment is recording all sounds in the marine
+            environment not just blasts, therefore to extract the blasts from
+            the many weeks of data automated computer algorithms are required.
+            All short, loud, low frequency sounds are extracted from the data by
+            the open source program{" "}
+            <a
+              href="https://www.pamguard.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={classes.linkText}
+            >
+              PamGuard
+            </a>
+            , producing thousands of clips.
           </Typography>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextArrowButton
-            className={classes}
-            text="FIND OUT MORE"
-            image={arrow}
-          />
+          <a
+            href="https://alpha.seasensors.africa/about/"
+            className={classes.arrowLink}
+          >
+            <TextArrowButton
+              className={classes}
+              text="FIND OUT MORE"
+              image={arrow}
+            />
+          </a>
         </Grid>
       </Grid>
     </div>
