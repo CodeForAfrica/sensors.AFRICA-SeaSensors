a5a36ccec44d2b9174ce7141f09eaff4124a1c54
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-06-10 16:03:24 +0300

    fix up partners

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -5,89 +5,98 @@ import { Grid, Typography, withStyles } from '@material-ui/core';
 import partners from '../assets/ourPartners.jpg';
 
 const styles = {
-  partnerParent: {
-    width: 'auto',
-    height: '750px',
-    backgroundColor: '#ffffff',
-    paddingTop: '112px'
+  root: {
+    padding: '1.875rem'
   },
   partnerImage: {
-    width: '500px',
-    height: '500px',
+    width: '31.25rem',
+    height: '31.25rem',
     display: 'inline',
     marginLeft: '20%',
     objectFit: 'cover'
   },
+  indentLine: {
+    width: '30px',
+    height: '0.8px',
+    marginRight: '22.5px',
+    backgroundColor: '#023256'
+  },
   ourPartners: {
-    width: '216.8px',
-    height: '24px',
+    width: '13.55rem',
     opacity: '0.4',
     fontFamily: 'Oswald',
-    fontSize: '10px',
+    fontSize: '0.625rem',
     fontWeight: '500',
     fontStyle: 'normal',
     fontStretch: 'normal',
     lineHeight: '1.65',
-    letterSpacing: '6px',
-    color: '#023256'
+    letterSpacing: '0.375rem',
+    color: '#023256',
+
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   thankYouText: {
     fontFamily: 'Oswald',
-    fontSize: '52px',
+    fontSize: '3.25rem',
     fontWeight: 'bold',
     fontStyle: 'normal',
     fontStretch: 'normal',
     lineHeight: '1.13',
-    letterSpacing: '0.7px',
-    color: '#023256'
+    letterSpacing: '0.0437rem',
+    color: '#023256',
+    marginBottom: '1.25rem'
   },
   seaSensorsText: {
     opacity: '0.6',
     fontFamily: 'Montserrat',
-    fontSize: '15px',
+    fontSize: '0.9375rem',
     fontWeight: 'normal',
     fontStyle: 'normal',
     fontStretch: 'normal',
     lineHeight: '2.5',
-    letterSpacing: '0.7px',
+    letterSpacing: '0.0437rem',
     textAlign: 'justify',
-    color: '#023256',
-    paddingRight: '122px',
-    paddingTop: '35px'
+    color: '#023256'
   }
 };
 
 function Partners({ classes }) {
   return (
-    <Grid container spacing={24} className={classes.partnerParent}>
-      <Grid item xs={12} sm={6}>
-        <img src={partners} className={classes.partnerImage} alt="" />
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} md={6}>
+          <img src={partners} className={classes.partnerImage} alt="" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="caption" className={classes.ourPartners}>
+            <div className={classes.indentLine} />
+            OUR PARTNERS
+          </Typography>
+          <Typography variant="h4" className={classes.thankYouText}>
+            A big thanks
+            {/*  br needed since design splits the text in this format */}
+            <br />
+            to all our partners.
+          </Typography>
+          <Typography className={classes.seaSensorsText}>
+            The project is headed by marine scientist, Gill Braulik, supported
+            by a seed grant from Code for Africa’s innovateAFRICA.fund. The
+            project would not be possible without the help of our partners,
+            Jamie Macaulay, St. Andrews University, Jason Rubens, Sound Ocean
+            Ltd, Tanzania Blast Monitoring Network, Mwambao Coastal Community
+            Network, Marine Parks and Reserves Unit, Tanzania Fisheries Research
+            Institute (TAFIRI), K15 Photos, africanDRONE and Nukta Africa Ltd.
+            Recorders have been deployed and retrieved with the assistance of
+            many, specifically: Fish Eagle Lodge, Kasa Divers, Unguja Lodge,
+            Seabreeze Marine, Emayani Beach Lodge, Friends of Maziwe, Peter
+            Olivier, Steve Attwell, Hannes Potgieter, Royce Yaezenko, and
+            Kerstin Erler.
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Typography variant="caption" className={classes.ourPartners}>
-          OUR PARTNERS
-        </Typography>
-        <Typography variant="h4" className={classes.thankYouText}>
-          A big thanks
-        </Typography>
-        <Typography variant="h4" className={classes.thankYouText}>
-          to all our partners.
-        </Typography>
-        <Typography className={classes.seaSensorsText}>
-          The project is headed by marine scientist, Gill Braulik, supported by
-          a seed grant from Code for Africa’s innovateAFRICA.fund. The project
-          would not be possible without the help of our partners, Jamie
-          Macaulay, St. Andrews University, Jason Rubens, Sound Ocean Ltd,
-          Tanzania Blast Monitoring Network, Mwambao Coastal Community Network,
-          Marine Parks and Reserves Unit, Tanzania Fisheries Research Institute
-          (TAFIRI), K15 Photos, africanDRONE and Nukta Africa Ltd. Recorders
-          have been deployed and retrieved with the assistance of many,
-          specifically: Fish Eagle Lodge, Kasa Divers, Unguja Lodge, Seabreeze
-          Marine, Emayani Beach Lodge, Friends of Maziwe, Peter Olivier, Steve
-          Attwell, Hannes Potgieter, Royce Yaezenko, and Kerstin Erler.
-        </Typography>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
 
