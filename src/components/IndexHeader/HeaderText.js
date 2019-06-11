889d472de29ecf4e58922415fc2047f4d683e901
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-06-11 14:49:14 +0300

    initial

--- src/components/IndexHeader/HeaderText.js
+++ src/components/IndexHeader/HeaderText.js
@@ -2,51 +2,61 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Typography } from '@material-ui/core';
 
-import arrow from '../../assets/arrowWhite.png';
+import TextArrowLink from '../TextArrowLink';
 
 const styles = {
-  content: {
+  root: {
     top: '176px',
-    padding: '4em 12em 2em 2em',
+    width: '80%',
+    height: '400px',
     display: 'flex',
     position: 'absolute',
     textAlign: 'left',
     flexDirection: 'column',
     justifyContent: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    marginRight: '40em'
+    overflow: 'hidden',
+    '&:before': {
+      content: '""',
+      background: 'inherit',
+      position: 'absolute',
+      left: '-25px',
+      top: '-25px',
+      right: 0,
+      bottom: 0,
+      boxShadow: 'inset 0 0 0 3000px rgba(255,255,255,0.3)',
+      filter: 'blur(4px)'
+    }
   },
-  contentHeader: {
+  content: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    padding: '30px'
+  },
+  title: {
     fontFamily: 'Oswald',
-    fontSize: '65px',
+    fontSize: '45px',
     fontWeight: 'bold',
     fontStyle: 'normal',
     fontStretch: 'normal',
-    lineHeight: '1.27',
-    letterSpacing: '0.8px',
+    lineHeight: 1.11,
+    letterSpacing: '0.6px',
     color: '#ffffff'
   },
-  contentSubHeader: {
-    fontFamily: 'Oswald',
-    fontSize: '2.2rem',
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: '1.27',
-    letterSpacing: '0.8px',
-    color: '#ffffff'
-  },
-  contentText: {
-    opacity: '0.6',
+  text: {
+    width: '272px',
+    height: '107px',
+    opacity: 0.6,
     fontFamily: 'Montserrat',
-    fontSize: '15px',
+    fontSize: '12px',
     fontWeight: 'normal',
     fontStyle: 'normal',
     fontStretch: 'normal',
-    lineHeight: '2.25',
-    letterSpacing: '0.7px',
-    marginRight: '5.2em',
-    color: '#ffffff'
+    lineHeight: 2.08,
+    letterSpacing: '0.5px',
+    textAlign: 'justify',
+    color: '#fff'
   },
   exploreMapParent: {
     display: 'flex',
@@ -65,47 +75,27 @@ const styles = {
     textAlign: 'right',
     color: '#ffffff',
     paddingRight: '20px'
-  },
-  arrowLink: {
-    textDecoration: 'none'
   }
 };
 
 function HeaderText({ classes }) {
   return (
-    <div className={classes.content}>
-      <Typography
-        component="h2"
-        variant="h1"
-        gutterBottom
-        className={classes.contentHeader}
-      >
-        Sea Sensors
-      </Typography>
-      <Typography
-        component="h3"
-        variant="h1"
-        gutterBottom
-        className={classes.contentSubHeader}
-      >
-        Mapping fishing with explosives along the coast of East Africa
-        <br />
-      </Typography>
-      <Typography variant="body1" gutterBottom className={classes.contentText}>
-        Using cutting-edge underwater acoustic technology to document the
-        occurrence of blast fishing.
-      </Typography>
-      <a
-        href="https://alpha.seasensors.africa/map/"
-        className={classes.arrowLink}
-      >
-        <span className={classes.exploreMapParent}>
-          <span className={classes.exploreMapText}>Explore map</span>
-          <span className={classes.exploreMapArrow}>
-            <img src={arrow} alt="Arrow" />
-          </span>
-        </span>
-      </a>
+    <div className={classes.root}>
+      <div className={classes.content}>
+        <Typography variant="h1" gutterBottom className={classes.title}>
+          Sea Sensors
+        </Typography>
+        <Typography variant="h1" gutterBottom className={classes.text}>
+          Mapping fishing with explosives along the coast of East Africa Using
+          cutting-edge underwater acoustic technology to document the occurrence
+          of blast fishing.
+        </Typography>
+        <TextArrowLink
+          border={false}
+          href="https://alpha.seasensors.africa/map/"
+          text="EXPLORE MAP"
+        />
+      </div>
     </div>
   );
 }
--- src/components/IndexHeader/SliderCarousel.js
+++ src/components/IndexHeader/SliderCarousel.js
@@ -8,21 +8,46 @@ import { withStyles } from '@material-ui/core';
 import HeaderText from './HeaderText';
 import Wave from './Wave';
 
-import HeaderImage1 from '../../assets/mapppp.png';
+import map from '../../assets/map.png';
 import HeaderImage2 from '../../assets/HeaderImage2.png';
-import gridImage from '../../assets/grid.png';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const styles = {
-  headerImage: {
-    background: 'no-repeat center center fixed',
-    WebkitBackgroundSize: 'cover',
-    MozBackgroundSize: 'cover',
-    OBackgroundSize: 'cover',
+const styles = theme => ({
+  root: {
+    height: '100vh',
+    '& .carousel-slider, .slider-wrapper, .slider': {
+      height: '100%'
+    },
+    [theme.breakpoints.up('md')]: {
+      height: '800px'
+    }
+  },
+  slide1: {
+    backgroundImage: `url(${map})`,
+    backgroundPosition: '-350px',
+    backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
-    minHeight: '100%',
-    height: '800px'
+    backgroundAttachment: 'fixed',
+    height: '100%',
+    '& img': {
+      objectFit: 'cover',
+      height: '100%'
+    },
+    [theme.breakpoints.up('md')]: {
+      backgroundPosition: 'unset'
+    }
+  },
+  slide2: {
+    backgroundImage: `url(${HeaderImage2})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    height: '100%',
+    '& img': {
+      objectFit: 'cover',
+      height: '100%'
+    }
   },
   grid: {
     position: 'absolute',
@@ -34,9 +59,9 @@ const styles = {
     justifyContent: 'flex-start',
     position: 'absolute',
     color: '#ffffff',
-    top: '43em',
     zIndex: '999',
-    left: '3.1em'
+    top: '40rem',
+    left: '3rem'
   },
   firstSlider: {
     marginRight: '8.5em'
@@ -49,45 +74,26 @@ const styles = {
     width: '100% !important',
     bottom: '1em !important'
   }
-};
-
-const Image = () => (
-  <div>
-    <img
-      src={gridImage}
-      style={{
-        height: 164,
-        width: 112,
-        position: 'fixed',
-        top: '8em',
-        right: '6em'
-      }}
-      alt="grid"
-    />
-  </div>
-);
+});
 
 function SliderCarousel(props) {
   const { classes } = props;
-  const { headerImage, grid, parentSlider, firstSlider } = classes;
   return (
-    <Carousel showStatus={false}>
-      <div>
-        <img src={HeaderImage1} alt="First Slide" className={headerImage} />
+    <Carousel className={classes.root} showStatus={false}>
+      {/* First Slide */}
+      <div className={classes.slide1}>
         <HeaderText />
-        <div className={grid}>
-          <Image />
-        </div>
-        <div className={parentSlider}>
-          <div className={firstSlider}>01</div>
+        <div className={classes.parentSlider}>
+          <div className={classes.firstSlider}>01</div>
           <div>02</div>
         </div>
       </div>
-      <div>
+      {/* Second Slide */}
+      <div className={classes.slide2}>
         <Wave />
-        <img src={HeaderImage2} alt="Second Slide" />
-        <div className={parentSlider}>
-          <div className={firstSlider}>01</div>
+        <img alt="" />
+        <div className={classes.parentSlider}>
+          <div className={classes.firstSlider}>01</div>
           <div>02</div>
         </div>
       </div>
