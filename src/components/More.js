aa321030595bc154e98ef89e81ac748b64c72d58
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-06-11 12:32:15 +0300

    Fixup Equipment and more (#34)
    * fix up top story component for desktop and mobile
    
    * fix up text sections for desktop and mobile
    
    * fix up equipment image
    
    * fix up more
    
    * fix up get involved
    
    * fix up partners
    
    * fix a big thanks

--- src/components/More.js
+++ src/components/More.js
@@ -4,69 +4,73 @@ import { Grid, Typography, withStyles } from '@material-ui/core';
 
 import TextArrowLink from './TextArrowLink';
 
-const styles = {
+const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    display: 'flex',
+    padding: '3.125rem 1.875rem',
+    paddingBottom: '6.75rem',
+    flexDirection: 'column',
+    [theme.breakpoints.up('md')]: {
+      minHeight: '31.25rem',
+
+      padding: '0 7.425rem',
+      paddingBottom: '5.0187rem',
+
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
   },
-  moreContainer: {
-    width: 'auto',
-    height: '30em',
-    backgroundColor: '#ffffff'
-  },
-  moreText: {
-    opacity: '0.6',
+  text: {
+    opacity: 0.6,
     fontFamily: 'Montserrat',
-    fontSize: '15px',
-    lineHeight: '2.5',
-    letterSpacing: '0.7px',
+    fontSize: '0.9375rem',
+    lineHeight: 2.5,
+    letterSpacing: '0.0437rem',
     textAlign: 'justify',
     color: '#023256',
-    marginTop: '125px',
-    marginLeft: '118.8px'
+    marginBottom: '2.5rem'
   },
   linkText: {
     fontFamily: 'Montserrat',
-    fontSize: '15px',
+    fontSize: '0.9375rem',
     color: '#023256',
     fontWeight: 'bold'
   },
-  readMore: {
-    width: '292.5px',
-    height: '75px',
-    boxShadow: '0 23px 60px 0 rgba(0, 0, 0, 0.05)'
+  findOutMore: {
+    [theme.breakpoints.up('md')]: {
+      alignSelf: 'flex-end'
+    }
   }
-};
+});
 
 function More({ classes }) {
   return (
-    <div className={classes.moreContainer}>
-      <Grid container spacing={24}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="body2" className={classes.moreText}>
-            The acoustic equipment is recording all sounds in the marine
-            environment not just blasts, therefore to extract the blasts from
-            the many weeks of data automated computer algorithms are required.
-            All short, loud, low frequency sounds are extracted from the data by
-            the open source program{' '}
-            <a
-              href="https://www.pamguard.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={classes.linkText}
-            >
-              PamGuard
-            </a>
-            , producing thousands of clips.
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextArrowLink
-            blue
-            text="FIND OUT MORE"
-            href="https://alpha.seasensors.africa/about/"
-          />
-        </Grid>
+    <div className={classes.root}>
+      <Grid item xs={12} md={6}>
+        <Typography variant="body2" className={classes.text}>
+          The acoustic equipment is recording all sounds in the marine
+          environment not just blasts, therefore to extract the blasts from the
+          many weeks of data automated computer algorithms are required. All
+          short, loud, low frequency sounds are extracted from the data by the
+          open source program{' '}
+          <a
+            href="https://www.pamguard.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes.linkText}
+          >
+            PamGuard
+          </a>
+          , producing thousands of clips.
+        </Typography>
       </Grid>
+      <TextArrowLink
+        classes={{ root: classes.findOutMore }}
+        blue
+        text="FIND OUT MORE"
+        href="https://alpha.seasensors.africa/about/"
+      />
     </div>
   );
 }
