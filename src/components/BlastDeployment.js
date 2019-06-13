56e3bb91fff5aa81e799cb5356ed3216e04c6a58
khadija <khadijamahanga@y7mail.com>
2019-06-13 11:10:56 +0300

    fix blast deployment component

--- src/components/BlastDeployment.js
+++ src/components/BlastDeployment.js
@@ -5,9 +5,14 @@ import { Typography, withStyles } from '@material-ui/core';
 import boat from '../assets/boat.png';
 import scubaDive from '../assets/ScubaDive.png';
 
-const styles = {
+const styles = theme => ({
   parentContainer: {
-    marginLeft: '90px'
+    marginTop: '68px',
+    marginLeft: 0,
+    [theme.breakpoints.up('md')]: {
+      marginLeft: '90px',
+      marginTop: '104.3px'
+    }
   },
   date: {
     width: '229.5px',
@@ -20,11 +25,11 @@ const styles = {
     fontStretch: 'normal',
     lineHeight: '4.75',
     letterSpacing: '6px',
-    color: ' #ffffff'
+    color: ' #ffffff',
+    marginBottom: '0.6rem'
   },
   blastDeploymentText: {
     opacity: '0.6',
-    paddingRight: '121px',
     fontFamily: 'Montserrat !important',
     fontSize: '15px !important',
     fontWeight: 'normal !important',
@@ -33,95 +38,129 @@ const styles = {
     lineHeight: '2.5 !important',
     letterSpacing: '0.7px !important',
     textAlign: 'justify',
-    color: '#023256 !important',
-    marginLeft: '792px',
-    position: 'relative'
+    color: '#023256 !important'
   },
   recordersText: {
-    width: '216.8px',
-    height: '114.5px',
+    width: '120.4px',
     fontFamily: 'Oswald',
-    fontSize: '30px',
+    fontSize: '16px',
     fontWeight: 'bold',
     fontStyle: 'normal',
     fontStretch: 'normal',
     lineHeight: '1.5',
-    letterSpacing: '1.2px',
-    color: '#ffffff'
+    letterSpacing: '0.6px',
+    color: '#ffffff',
+    [theme.breakpoints.up('md')]: {
+      letterSpacing: '1.2px',
+      fontSize: '30px',
+      width: '225.8px'
+    }
   },
   scubaDive: {
     width: '100%',
     position: 'relative',
-    height: '720px',
-    objectFit: 'cover',
-    objectPosition: '0 0'
+    height: '467px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '80%',
+    top: '0',
+    left: '0',
+    backgroundImage: `url(${scubaDive})`,
+    [theme.breakpoints.up('md')]: {
+      height: '720px',
+      backgroundPosition: '50% 35%'
+    }
   },
   boat: {
-    position: 'absolute',
-    bottom: '300px',
-    width: '510px',
-    height: '630px',
-    left: '90px',
-    top: '580.5px'
+    position: 'relative',
+    height: '378px',
+    left: 0,
+    width: '306px',
+    marginTop: '-60px',
+    [theme.breakpoints.up('md')]: {
+      height: '630px',
+      left: '-150px',
+      width: '510px',
+      top: '580.5px',
+      position: 'absolute',
+      bottom: '300px'
+    }
   },
   recordDeploy: {
-    right: '102px',
-    bottom: '85px',
-    position: 'absolute'
+    right: '30px',
+    bottom: '120px',
+    position: 'absolute',
+    width: '115px',
+    [theme.breakpoints.up('md')]: {
+      right: '102px',
+      width: '216.8px'
+    }
   },
   scubaParent: {
     position: 'relative',
-    paddingLeft: '150px',
-    height: '720px'
+    width: '100%',
+    height: 'auto',
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
+      marginLeft: '150px'
+    }
   },
   textContainer: {
-    paddingTop: '153px'
+    paddingTop: '63px',
+    paddingLeft: '30px',
+    paddingRight: '30px',
+    [theme.breakpoints.up('md')]: {
+      paddingTop: '153px',
+      marginLeft: '480px',
+      position: 'relative'
+    }
   }
-};
+});
 
 function BlastDeployment({ classes }) {
   return (
     <div className={classes.parentContainer}>
       <div className={classes.scubaParent}>
-        <img src={scubaDive} alt="Scuba dive" className={classes.scubaDive} />
-        <div className={classes.recordDeploy}>
-          <Typography variant="body1" gutterBottom className={classes.date}>
-            24.09.2017
+        <div className={classes.scubaDive}>
+          <div className={classes.recordDeploy}>
+            <Typography variant="body1" gutterBottom className={classes.date}>
+              24.09.2017
+            </Typography>
+            <Typography
+              variant="body1"
+              gutterBottom
+              className={classes.recordersText}
+            >
+              Deployment of Blast Recorders
+            </Typography>
+          </div>
+        </div>
+        <img src={boat} alt="Boat" className={classes.boat} />
+        <div className={classes.textContainer}>
+          <Typography
+            variant="body1"
+            gutterBottom
+            className={classes.blastDeploymentText}
+          >
+            The acoustic monitoring focuses on the two areas of Tanzania that
+            are well known to have high levels of blasting activity over many
+            years: Dar es Salaam, and Tanga. Equipment was constructed using
+            local materials where possible. A triangular metal frame was welded
+            by local craftsmen in Zanzibar, heavy weights to hold the acoustic
+            equipment to the sea bed were used gerry cans that previously held
+            cooking oil, that were filled with concrete.
           </Typography>
           <Typography
             variant="body1"
             gutterBottom
-            className={classes.recordersText}
+            className={classes.blastDeploymentText}
           >
-            Deployment of Blast Recorders
+            The metal frame was wrapped in used bicycle inner tubes to prevent
+            abrasion to the sensitive equipment (add photos). Finally the gear
+            was transported via truck, high speed ferry, and boat to the
+            deployment location
           </Typography>
         </div>
-        <img src={boat} alt="Boat" className={classes.boat} />
-      </div>
-      <div className={classes.textContainer}>
-        <Typography
-          variant="body1"
-          gutterBottom
-          className={classes.blastDeploymentText}
-        >
-          The acoustic monitoring focuses on the two areas of Tanzania that are
-          well known to have high levels of blasting activity over many years:
-          Dar es Salaam, and Tanga. Equipment was constructed using local
-          materials where possible. A triangular metal frame was welded by local
-          craftsmen in Zanzibar, heavy weights to hold the acoustic equipment to
-          the sea bed were used gerry cans that previously held cooking oil,
-          that were filled with concrete.
-        </Typography>
-        <Typography
-          variant="body1"
-          gutterBottom
-          className={classes.blastDeploymentText}
-        >
-          The metal frame was wrapped in used bicycle inner tubes to prevent
-          abrasion to the sensitive equipment (add photos). Finally the gear was
-          transported via truck, high speed ferry, and boat to the deployment
-          location
-        </Typography>
       </div>
     </div>
   );
