578211822fc8c0949ded5d5ef6f7d8792b49a99a
nerdlet <brndnyokabi@gmail.com>
2019-06-28 11:31:54 +0300

    Add preloader circle

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "SeaSensors | sensors.AFRICA"
+    title: 'SeaSensors | sensors.AFRICA'
   },
   plugins: [
     {
@@ -10,21 +10,30 @@ module.exports = {
         short_name: `SeaSensors`,
         icons: [
           {
-            "src": "/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            "src": "/android-chrome-512x512.png",
-             "sizes": "512x512",
-             "type": "image/png"
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ],
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
-        display: `standalone`,
-      },
+        display: `standalone`
+      }
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true
+      }
+    }
   ]
 };
